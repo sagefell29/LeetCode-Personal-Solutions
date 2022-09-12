b9f5c3310268b0faa9b7786070c2bb685f33fbad
Rishi Kaushal <rishi29work@gmail.com>
2022-09-12 22:33:18 +0530

    Time: 223 ms (11.05%), Space: 51.4 MB (52.33%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> ans;
-        for (int i:nums) ans[i]++;
-        for (auto i:ans) if (i.second>1) return true;
+        for (int i:nums) 
+        {
+            ans[i]++;
+            if (ans[i]>1) return true;
+        }
+        
         return false;
     }
 };
