9854fdb9417935dec141b1a84d6e01021f90f24c
Rishi Kaushal <rishi29work@gmail.com>
2022-09-19 21:20:46 +0530

    Time: 17 ms (72.12%), Space: 12.9 MB (18.09%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:    
+   
+    static bool cmp(pair<string,int> a, pair<string,int> b){
+    if (a.second>b.second) return true;
+    else if (a.second==b.second) return a.first<b.first;
+    return false;
+}
+
+
+vector<string> topKFrequent(vector<string>& words, int k) 
+{
+    unordered_map<string,int>ump;
+    for(auto i:words) ump[i]++;
+    vector<pair<string,int>>v;
+    for(auto i:ump) v.push_back(i);
+    sort(v.begin(),v.end(),cmp);
+    vector<string>ans;
+    for(int i=0;i<k;i++) ans.push_back(v[i].first);
+    return ans;
+    }
+};
