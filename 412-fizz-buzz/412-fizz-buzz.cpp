3b2eea9310a926f5475e53eb811c59ed3d745c68
Rishi Kaushal <rishi29work@gmail.com>
2022-10-05 15:08:15 +0530

    Time: 3 ms (88.97%), Space: 7.9 MB (64.07%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        int i=1;
+        vector<string> ans;
+        while (i<=n)
+        {
+            if (i%3==0 and i%5==0) ans.push_back("FizzBuzz");
+            else if (i%3==0) ans.push_back("Fizz");
+            else if (i%5==0) ans.push_back("Buzz");
+            else ans.push_back(to_string(i));
+            i++;
+        }
+        return ans;
+    }
+};
